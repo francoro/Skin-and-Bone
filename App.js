16940b0c701b68d6d48f79217d1cfe0f2723bc4d
franconicolascoronel94 <franco_@openmailbox.org>
2018-05-12 19:58:08 -0300

    icons working

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
 
 import Login from './src/pages/login/login';
 import Home from './src/pages/home/home';
 import Profile from './src/pages/profile/profile';
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
 export default class App extends Component {
+  
+
   render() {
+    const TabIcon = props  => {
+      var color = props.focused ? 'red' : 'black';
+       return (
+        <View style={{flex:1, flexDirection:'column', alignItems:'center', alignSelf:'center', justifyContent: 'center'}}>
+        <Icon style={{color: color}} name={props.iconName} size={18}/>
+        <Text style={{color: color, fontSize: 12}}>{props.title}</Text>
+      </View>
+      ); 
+    }
+
     return (
       <Router>
         <Scene key="root" hideNavBar>
@@ -22,11 +37,11 @@ export default class App extends Component {
           <Scene
             key="tabbar"
             tabs={true}
-            tabBarStyle={{ backgroundColor: 'red' }}
+            tabBarStyle={{ backgroundColor: 'gray' }}
             tabBarPosition={'bottom'}
           >
-            {/* Tab and it's scenes */}
-            <Scene key="tabhome" title="Inicio">
+            
+            <Scene key="tabhome" title="Inicio" icon={TabIcon} iconName="md-home">
               <Scene
                 key="home"
                 component={Home}
@@ -34,8 +49,8 @@ export default class App extends Component {
               />
             </Scene>
 
-            {/* Tab and it's scenes */}
-            <Scene key="tabprofile" title="Perfil">
+            
+            <Scene key="tabprofile" title="Perfil" icon={TabIcon} iconName="md-person">
               <Scene
                 key="profile"
                 component={Profile}
