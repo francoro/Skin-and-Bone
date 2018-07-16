9095dc387c3ebc4915608b4269fc2e9e9de2e94e
Franco Coronel <francocoronel@Francos-MBP.home>
2018-07-15 21:08:31 -0300

    styling card

--- src/postItem.js
+++ src/postItem.js
@@ -4,22 +4,31 @@ import { View, Image, FlatList, Text, Dimensions, TouchableOpacity, StyleSheet }
 
 export default class PostItem extends Component {
     render() {
+        let tagType;
+        switch (this.props.item.type) {
+            case 1:
+                tagType = "Encontrados";
+                break;
+            case 2:
+                tagType = "Perdidos";
+                break;
+            case 3:
+                tagType = "Adopcion";
+                break;
+        }
+
         return (
-            <View styles={styles.container}>
-                <View styles={styles.userContainer}>
-                    <View styles={styles.userImgContainer}>
-                        <Image style={styles.userImg} source={{ uri: this.props.item.user.picture }} />
-                    </View>
-                    <View styles={styles.textContainer}>
-                        <View styles={styles.nameContainer}>
-                            <Text style={styles.name}>{this.props.item.user.name}</Text>
-                        </View>
-                        <View styles={styles.dateContainer}>
-                            <Text style={styles.date}>{this.props.item.created}</Text>
-                        </View>
+            <View style={styles.container}>
+                <View style={styles.topContainer}>
+                    <Image style={styles.userImg} source={{ uri: this.props.item.user.picture }} />
+                    <View style={styles.infoContainer}>
+                        <Text style={styles.name}>{this.props.item.user.name}</Text>
+                        <Text style={styles.date}>{this.props.item.created}</Text>
                     </View>
                 </View>
 
+                <Text style={styles.body}>{this.props.item.body}</Text>
+                <View><Text style={styles.tag}>{tagType}</Text></View>
                 <Image style={{ width: Dimensions.get('window').width, height: 400 }} source={{ uri: this.props.item.image }} />
             </View>
         );
@@ -29,35 +38,47 @@ export default class PostItem extends Component {
 const styles = StyleSheet.create({
     container: {
         backgroundColor: "white",
-        marginBottom: 20,
-        paddingHorizontal: 20
+        marginBottom: 20
     },
-    userContainer: {
+    topContainer: {
         flex: 1,
-        flexDirection: 'row'
+        flexDirection: "row"
     },
-    userImgContainer: {
-        marginLeft: 10
+    infoContainer: {
+        flex: 1,
+        flexDirection: "column",
+        marginTop: 20
     },
     userImg: {
         borderRadius: 50,
-        width: 40,
-        height: 40
-    },
-    textContainer: {
-        marginLeft: 5
-    },
-    nameContainer: {
-
-    },
-    dateContainer: {
-
+        width: 55,
+        height: 55,
+        marginLeft: 10,
+        marginTop: 10
     },
     name: {
+        marginLeft: 10,
         fontWeight: "600",
         color: "black"
     },
     date: {
-        color: "#f2f2f2"
+        color: "#999",
+        marginLeft: 10
+    },
+    body: {
+        paddingVertical: 20,
+        paddingHorizontal: 12
+    },
+    tagContainer: {
+        
+    },
+    tag: {
+        borderRadius: 6,
+        borderColor: "#999",
+        borderWidth: 2,
+        padding: 5,
+        marginBottom: 10,
+        fontWeight: "600",
+        color: "#999"
     }
 })
