dd5f4a5c17781b116b8a453292d079c294b5a5dd
Franco Coronel <francocoronel@Francos-MacBook-Pro.local>
2018-09-09 17:13:08 -0300

    footer new post styled

--- src/pages/new-post/newPost.js
+++ src/pages/new-post/newPost.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, TouchableWithoutFeedback, TextInput, ScrollView } from 'react-native';
+import { Text, View, StyleSheet, TouchableWithoutFeedback, TextInput, ScrollView, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+const { width } = Dimensions.get('window');
 export default class Login extends Component {
     constructor(props) {
         super(props);
@@ -16,8 +17,9 @@ export default class Login extends Component {
 
     render() {
         return (
-            <ScrollView contentContainerStyle={{ flexGrow: 1 }}
-            keyboardShouldPersistTaps='handled' style={styles.containerNewPost}>
+            <View style={{ flex: 1 }}>
+                <ScrollView contentContainerStyle={{ flexGrow: 1 }}
+                    keyboardShouldPersistTaps='handled' style={styles.containerNewPost}>
                     <Text style={styles.titleColor}>DESCRIPCION</Text>
                     <TextInput
                         style={styles.textArea}
@@ -25,37 +27,48 @@ export default class Login extends Component {
                         textAlignVertical="top"
                         underlineColorAndroid="transparent"
                     />
-                <View style={styles.containerType}>
-                    <Text style={styles.titleColor}>TIPO DE PUBLICACION</Text>
-                    <View style={styles.checkboxContainer}>
-                        <View>
-                            <TouchableWithoutFeedback onPress={() => this.selectOption(1)}>
-                                <View style={[styles.checkboxItem, styles.firstItem, this.state.option === 1 ? styles.activeCheck : null]}>
-                                    <Text style={styles.optionCheckText}>En adopcion</Text>
-                                    {this.state.option === 1 ? <Icon name="md-checkmark" style={styles.iconCheck} color="#F5DA49" size={33} /> : null}
-                                </View>
-                            </TouchableWithoutFeedback>
+                    <View style={styles.containerType}>
+                        <Text style={styles.titleColor}>TIPO DE PUBLICACION</Text>
+                        <View style={styles.checkboxContainer}>
+                            <View>
+                                <TouchableWithoutFeedback onPress={() => this.selectOption(1)}>
+                                    <View style={[styles.checkboxItem, styles.firstItem, this.state.option === 1 ? styles.activeCheck : null]}>
+                                        <Text style={styles.optionCheckText}>En adopcion</Text>
+                                        {this.state.option === 1 ? <Icon name="md-checkmark" style={styles.iconCheck} color="#F5DA49" size={33} /> : null}
+                                    </View>
+                                </TouchableWithoutFeedback>
+                            </View>
+                            <View>
+                                <TouchableWithoutFeedback onPress={() => this.selectOption(2)}>
+                                    <View style={[styles.checkboxItem, this.state.option === 2 ? styles.activeCheck : null]}>
+                                        <Text style={styles.optionCheckText}>Encontrado</Text>
+                                        {this.state.option === 2 ? <Icon name="md-checkmark" style={styles.iconCheck} color="#F5DA49" size={33} /> : null}
+                                    </View>
+                                </TouchableWithoutFeedback>
+                            </View>
+                            <View>
+                                <TouchableWithoutFeedback onPress={() => this.selectOption(3)}>
+                                    <View style={[styles.checkboxItem, this.state.option === 3 ? styles.activeCheck : null]}>
+                                        <Text style={styles.optionCheckText}>Perdido</Text>
+                                        {this.state.option === 3 ? <Icon name="md-checkmark" style={styles.iconCheck} color="#F5DA49" size={33} /> : null}
+                                    </View>
+                                </TouchableWithoutFeedback>
+                            </View>
                         </View>
-                        <View>
-                            <TouchableWithoutFeedback onPress={() => this.selectOption(2)}>
-                                <View style={[styles.checkboxItem, this.state.option === 2 ? styles.activeCheck : null]}>
-                                    <Text style={styles.optionCheckText}>Encontrado</Text>
-                                    {this.state.option === 2 ? <Icon name="md-checkmark" style={styles.iconCheck} color="#F5DA49" size={33} /> : null}
-                                </View>
-                            </TouchableWithoutFeedback>
-                        </View>
-                        <View>
-                            <TouchableWithoutFeedback onPress={() => this.selectOption(3)}>
-                                <View style={[styles.checkboxItem, this.state.option === 3 ? styles.activeCheck : null]}>
-                                    <Text style={styles.optionCheckText}>Perdido</Text>
-                                    {this.state.option === 3 ? <Icon name="md-checkmark" style={styles.iconCheck} color="#F5DA49" size={33} /> : null}
-                                </View>
-                            </TouchableWithoutFeedback>
-                        </View>
-                    </View>
 
+                    </View>
+                </ScrollView>
+                <View style={styles.footer}>
+                    <View style={styles.buttonFooter}>
+                        <Icon name="md-camera" style={styles.iconImage} color="#979797" size={33} />
+                        <Text style={styles.textFooter}>TOMAR FOTO </Text>
+                    </View>
+                    <View style={styles.buttonFooter}>
+                        <Icon name="md-image" style={styles.iconImage} color="#979797" size={33} />
+                        <Text style={styles.textFooter}>ABRIR GALERIA </Text>
+                    </View>
                 </View>
-            </ScrollView>
+            </View>
         );
     }
 }
@@ -99,5 +112,22 @@ const styles = StyleSheet.create({
         height: 150,
         justifyContent: "flex-start",
         paddingHorizontal: 20
+    },
+    footer: {
+        backgroundColor: "#fff",
+        flexDirection: "row",
+        paddingVertical: 15,
+        paddingHorizontal: 20
+    },
+    buttonFooter: {
+        width: width / 2,
+        flexDirection: "row"
+    },
+    textFooter: {
+        fontWeight: "600",
+        marginLeft: 15,
+        position: "relative",
+        top: 7
     }
+
 })
